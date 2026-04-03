d50973873afed1bf4ed9734a42d41462b4aa327e
luoyufeng <1443724970@qq.com>
2026-04-03 16:08:25 +0800

    chore: 停止上传dist文件夹

--- packages/vi/dist/theme-drawer.js
+++
@@ -1,6 +0,0 @@
-import { T as r } from "./theme-drawer-DYc-OGJJ.js";
-export {
-  r as ThemeDrawer,
-  r as default
-};
-//# sourceMappingURL=theme-drawer.js.map
--- packages/vi/dist/theme.js
+++
@@ -1,6 +0,0 @@
-import "vue";
-import { u as r } from "./use-vi-theme-BbYqO0Nd.js";
-export {
-  r as useViTheme
-};
-//# sourceMappingURL=theme.js.map
